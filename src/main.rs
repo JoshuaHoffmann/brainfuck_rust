68532f5ab06e989a79507211d8692122e6ee0dd5
JoshuaHoffmann <pot.coffee420@yandex.com>
2021-08-24 03:04:49 +0200

    added: error message

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 2 {
+        println!("Please provide only one filename.");
         print_usage();
         std::process::exit(0);
     }
